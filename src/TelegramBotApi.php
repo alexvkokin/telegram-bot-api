b280916e02f45ffc408b40b8b7114a82a80f0f22
alexvkokin <alexvkokin@gmail.com>
2024-10-09 09:10:39 +0300

    Рефакторинг

--- src/TelegramBotApi.php
+++ src/TelegramBotApi.php
@@ -5,6 +5,7 @@ namespace Alexvkokin\TelegramBotApi;
 
 use Alexvkokin\TelegramBotApi\Client\Client;
 use Alexvkokin\TelegramBotApi\Client\Request;
+use Alexvkokin\TelegramBotApi\Client\Response;
 use Alexvkokin\TelegramBotApi\Method\Method;
 use Alexvkokin\TelegramBotApi\Parser\MethodParser;
 use Alexvkokin\TelegramBotApi\Parser\ResponseParser;
@@ -17,22 +18,32 @@ use Throwable;
 
 final readonly class TelegramBotApi
 {
-    private string $baseUrl;
-
     public function __construct(
-        string $token,
+        private string $token,
         private Client $client,
-        string $baseUrl = 'https://api.telegram.org',
+        private string $baseUrl = 'https://api.telegram.org',
         private MethodParser $methodParser = new MethodParser(),
         private ResponseParser $responseParser = new ResponseParser(),
-    ) {
-        $this->baseUrl = $baseUrl . '/bot' . $token . '/';
-    }
+    ) {}
 
     /**
      * @throws RuntimeException
      */
     public function send(Method $method): Type
+    {
+        $response = $this->request($method);
+
+        [$fieldOk, $fieldResult] = $this->decodeBody($response->body);
+
+        if ($fieldOk === true) {
+            return $this->getResult($method, $fieldResult);
+        }
+
+        return $this->getFailureResult();
+    }
+
+
+    private function request(Method $method): Response
     {
         try {
             $request = new Request(
@@ -40,42 +51,48 @@ final readonly class TelegramBotApi
                 $method->method(),
                 $this->methodParser->getParams($method),
             );
-            $response = $this->client->send($request);
+
+            return $this->client->send($request);
 
         } catch (ClientExceptionInterface $e) {
             throw new RuntimeException('Error sending request.', previous: $e);
         }
+    }
 
+    private function getUrl(Method $method): string
+    {
+        return $this->baseUrl . '/bot' . $this->token . '/' . $this->methodParser->getName($method);
+    }
+
+    private function decodeBody(string $body): array
+    {
         try {
-            $body = json_decode($response->body, true, flags: JSON_THROW_ON_ERROR);
+            $decodedBody = json_decode($body, true, flags: JSON_THROW_ON_ERROR);
         } catch (JsonException $e) {
             throw new RuntimeException('Error parsing response.', previous: $e);
         }
 
-        $fieldOk = $body['ok'] ?? null;
+        $fieldOk = $decodedBody['ok'] ?? null;
+        $fieldResult = $decodedBody['result'] ?? null;
 
         if ($fieldOk === null) {
             throw new RuntimeException('Incorrect "ok" field in response. Expected boolean, got ' . gettype($fieldOk));
         }
 
-        if ($fieldOk === true) {
-            try {
-                return $this->responseParser->asObject($method->responseClassName(), $body['result'] ?? []);
-            } catch (Throwable $e) {
-                throw new RuntimeException('Error parsing response.', previous: $e);
-            }
-        }
+        return [$fieldOk, $fieldResult];
+    }
 
-        return $this->getFailureResult();
+    private function getResult(Method $method, array $result): Type
+    {
+        try {
+            return $this->responseParser->asObject($method->responseClassName(), $result);
+        } catch (Throwable $e) {
+            throw new RuntimeException('Error parsing response.', previous: $e);
+        }
     }
 
     private function getFailureResult(): Type
     {
         return new FailResponse();
-    }
-
-    private function getUrl(Method $method): string
-    {
-        return $this->baseUrl . $this->methodParser->getName($method);
     }
 }
